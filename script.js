975821a05ed6ff83613a26ac9fd978ab5277d8a8
Goji Goji <arturikkaral@gmail.com>
2023-05-19 13:22:31 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.code === "Space") {
+        startGame();
+    }
+})
+
+    const polka = document.getElementById("polka");
+    const cactus = document.getElementById("cactus");
+    const scoreDisplay = document.getElementById("score");
+
+    let count = 0;
+    let speed = 1;
+
+    document.addEventListener("keydown", function(event) {
+        jump();
+    });
+
+function jump () {
+    if (polka.classList!= "jump") {
+        polka.classList.add("jump")
+    }
+    setTimeout ( function() {
+        polka.classList.remove("jump")
+        count += 100;
+        scoreDisplay.textContent = count;
+
+        if (count % 300 == 0 && speed > 0) {
+            speed -= 0.01;
+            cactus.style.setProperty("animation", `cactusMov ${speed}s  infinite linear`);
+            
+        }
+    
+        // // // if (count % 300 == 0 && speed > 0) {
+        // //     speed -= 0.01;
+        // //    {
+        // //     esle (`cactusMov ${speed}s += 0.1s infinite linear `)
+        //    }
+    }, 300) 
+}
+
+let isAlive = setInterval(function() { 
+    let polkaTop = parseInt(window.getComputedStyle(polka).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left")); 
+
+    if (cactusLeft < 50 && cactusLeft > 0 && polkaTop >= 140) {
+        alert("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!SMOKE!!SMOKE!!SMOKE!!!SMOKE!!!SMOKE!!!!SMOKE!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ")
+        clearInterval(isAlive);
+        location.reload();
+        
+    } 
+    cactus.style.left = (parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))) + "px";
+    
+   
+    
+}, 10);
