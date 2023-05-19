9ff3cf167bbb8392a33e947a1551f86c2a66ffb2
Goji Goji <arturikkaral@gmail.com>
2023-05-19 17:18:20 +0300

    slayberg

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 
     let count = 0;
     let speed = 1;
+    let isPaused = true;
 
     document.addEventListener("keydown", function(event) {
         jump();
@@ -32,39 +33,38 @@ function jump () {
         //    }
     }, 300) 
 }
-
-function startGame() {
-    const startButton = document.getElementById("start-button")
-    const startScreen = document.getElementById("start-screen")
-
-    let isPaused = true;
-
-    startButton.addEventListener("click", function() { 
-
-        if (isPaused) {
-            
-            isPaused = false;
-
-        } else {
-            return;
-        }
-
-    })
-}
-
-
-let isAlive = setInterval(function() { 
-    let polkaTop = parseInt(window.getComputedStyle(polka).getPropertyValue("top"));
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left")); 
-
-    if (cactusLeft < 50 && cactusLeft > 0 && polkaTop >= 140) {
-        alert("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!SMOKE!!SMOKE!!SMOKE!!!SMOKE!!!SMOKE!!!!SMOKE!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ")
+function resetGame() {
+    count = 0;
+    speed = 1;
+    scoreDisplay.textContent = count;
+    cactus.style.setProperty("animation", "none");
+    polka.style.top = "140px";
+  }
+  
+  function startGame() {
+    const startButton = document.getElementById("start-button");
+    const startScreen = document.getElementById("start-screen");
+  
+    startButton.addEventListener("click", function() {
+      isPaused = !isPaused; 
+      // Переключение состояния паузы
+      if (!isPaused) {
+        "startButton"
+      }
+    });
+  }
+  
+  let isAlive = setInterval(function() {
+    
+      let polkaTop = parseInt(window.getComputedStyle(polka).getPropertyValue("top"));
+      let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+  
+      if (cactusLeft < 50 && cactusLeft > 0 && polkaTop >= 140) {
+        alert("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!SMOKE!!SMOKE!!SMOKE!!!SMOKE!!!SMOKE!!!!SMOKE!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ");
         clearInterval(isAlive);
-        location.reload();
-        
-    } 
-    cactus.style.left = (parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))) + "px";
+        resetGame();
+        startGame(); // Сброс игры к начальному состоянию
+      }
+      cactus.style.left = (parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))) + "px";
     
-   
-    
-}, 10);
+  }, 10);
